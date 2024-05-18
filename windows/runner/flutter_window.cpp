b28ac27706b65067d91199cd1db0dc4f4b294164
Muhammad Rafay <k200180@nu.edu.pk>
2024-05-18 18:34:00 +0500

    first commit

---
+++ windows/runner/flutter_window.cpp
@@ -0,0 +1,119 @@
+#include "flutter_window.h"
+
+#include <optional>
+
+#include "flutter/generated_plugin_registrant.h"
+#include <flutter/event_channel.h>
+#include <flutter/event_sink.h>
+#include <flutter/event_stream_handler_functions.h>
+#include <flutter/method_channel.h>
+#include <flutter/standard_method_codec.h>
+#include <windows.h>
+
+#include <memory>
+static int GetBatteryLevel()
+{
+  SYSTEM_POWER_STATUS status;
+  if (GetSystemPowerStatus(&status) == 0 || status.BatteryLifePercent == 255)
+  {
+    return -1;
+  }
+  return status.BatteryLifePercent;
+}
+
+FlutterWindow::FlutterWindow(const flutter::DartProject &project)
+    : project_(project) {}
+
+FlutterWindow::~FlutterWindow() {}
+
+bool FlutterWindow::OnCreate()
+{
+  if (!Win32Window::OnCreate())
+  {
+    return false;
+  }
+
+  RECT frame = GetClientArea();
+
+  // The size here must match the window dimensions to avoid unnecessary surface
+  // creation / destruction in the startup path.
+  flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
+      frame.right - frame.left, frame.bottom - frame.top, project_);
+  // Ensure that basic setup of the controller was successful.
+  if (!flutter_controller_->engine() || !flutter_controller_->view())
+  {
+    return false;
+  }
+  RegisterPlugins(flutter_controller_->engine());
+  flutter::MethodChannel<> channel(
+      flutter_controller_->engine()->messenger(), "samples.flutter.dev/battery",
+      &flutter::StandardMethodCodec::GetInstance());
+  channel.SetMethodCallHandler(
+      [](const flutter::MethodCall<> &call, std::unique_ptr<flutter::MethodResult<>> result)
+      {
+        if (call.method_name() == "getBatteryLevel")
+        {
+          int battery_level = GetBatteryLevel();
+          if (battery_level != -1)
+          {
+            result->Success(battery_level);
+          }
+          else
+          {
+            result->Error("UNAVAILABLE", "Battery level not available.");
+          }
+        }
+        else
+        {
+          result->NotImplemented();
+        }
+      });
+  SetChildContent(flutter_controller_->view()->GetNativeWindow());
+
+  flutter_controller_->engine()->SetNextFrameCallback([&]()
+                                                      { this->Show(); });
+
+  // Flutter can complete the first frame before the "show window" callback is
+  // registered. The following call ensures a frame is pending to ensure the
+  // window is shown. It is a no-op if the first frame hasn't completed yet.
+  flutter_controller_->ForceRedraw();
+
+  return true;
+}
+
+void FlutterWindow::OnDestroy()
+{
+  if (flutter_controller_)
+  {
+    flutter_controller_ = nullptr;
+  }
+
+  Win32Window::OnDestroy();
+}
+
+LRESULT
+FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
+                              WPARAM const wparam,
+                              LPARAM const lparam) noexcept
+{
+  // Give Flutter, including plugins, an opportunity to handle window messages.
+  if (flutter_controller_)
+  {
+    std::optional<LRESULT> result =
+        flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
+                                                      lparam);
+    if (result)
+    {
+      return *result;
+    }
+  }
+
+  switch (message)
+  {
+  case WM_FONTCHANGE:
+    flutter_controller_->engine()->ReloadSystemFonts();
+    break;
+  }
+
+  return Win32Window::MessageHandler(hwnd, message, wparam, lparam);
+}
